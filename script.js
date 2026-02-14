b95ec4b2fdd2436a850e3e5da6a56097107258a8
Bheesham505 <bheeshamkumarsajnani3@gmail.com>
2026-02-14 18:56:11 +0500

    .

---
+++ script.js
@@ -0,0 +1,23 @@
+// Arrow Functions
+
+// const square = (num) => {
+//     return num * num;
+// }
+
+// const square1 = num => num * num;
+
+
+// const result = square(5);
+// const result1 = square1(5);
+// console.log(result); 
+// console.log(result1);
+
+// // Arrow function with multiple parameters
+// const add = (a, b) => a + b;
+// const sum = add(3, 4);
+// console.log(sum);
+
+// Arrow function with no parameters
+// const greet = () => "Hello, World!";
+// console.log(greet());
+
